06aef8a62732d6bde7fae37ebc2790478f2caba0
Josh Noel <obsidize@gmail.com>
2021-02-15 21:42:19 -0600

    add complete coverage for LogEvent

--- tests/log-event.spec.ts
+++ tests/log-event.spec.ts
@@ -1,12 +1,62 @@
-import { LogEvent, LogLevel } from '../src/index';
+import { LogEvent, LogLevel, RxConsole } from '../src/index';
 
 describe('LogEvent', () => {
 
-	it('has assorted tools for creating and displaying event data', () => {
-		const now = new Date();
-		const sampleEvent = new LogEvent(LogLevel.DEBUG, 'test message', [], 'custom-tag', now.getTime());
-		expect(sampleEvent.toString()).toBe(`${now.toJSON()} [DEBUG] [custom-tag] test message`);
-		const sampleEvent2 = new LogEvent(LogLevel.DEBUG, 'another message', [{ testValue: true }], 'custom-tag', now.getTime());
-		expect(sampleEvent2.toString()).toBe(`${now.toJSON()} [DEBUG] [custom-tag] another message :: {"testValue":true}`);
+	describe('toString()', () => {
+
+		it('dumps the event data using standard formatting defaults', () => {
+
+			const now = new Date();
+
+			const sampleEvent1 = new LogEvent(LogLevel.DEBUG, 'test message', [], 'custom-tag', now.getTime());
+			const logMessage1 = `${now.toJSON()} [DEBUG] [custom-tag] test message`;
+			expect(sampleEvent1.toString()).toBe(logMessage1);
+
+			const sampleEvent2 = new LogEvent(LogLevel.DEBUG, 'another message', [{ testValue: true }], 'custom-tag', now.getTime());
+			const logMessage2 = `${now.toJSON()} [DEBUG] [custom-tag] another message :: {"testValue":true}`;
+			expect(sampleEvent2.toString()).toBe(logMessage2);
+		});
+	});
+
+	describe('broadcast() and broadcastTo()', () => {
+
+		it('routes the given LogEvent to its nearest ConsoleLike equivalent', () => {
+
+			const now = new Date();
+			const { mockConsole } = RxConsole;
+
+			const sampleEvent1 = new LogEvent(LogLevel.DEBUG, 'test message', [], 'custom-tag', now.getTime());
+			spyOn(mockConsole, 'log').and.callThrough();
+			sampleEvent1.broadcastTo(mockConsole);
+			expect(mockConsole.log).toHaveBeenCalledWith(sampleEvent1.message);
+
+			const sampleEvent2 = new LogEvent(LogLevel.WARN, 'test warning', [], 'custom-tag', now.getTime());
+			spyOn(mockConsole, 'warn').and.callThrough();
+			sampleEvent2.broadcastTo(mockConsole);
+			expect(mockConsole.warn).toHaveBeenCalledWith(sampleEvent2.message);
+
+			const sampleEvent3 = new LogEvent(LogLevel.FATAL, 'test error', [], 'custom-tag', now.getTime());
+			spyOn(mockConsole, 'error').and.callThrough();
+			sampleEvent3.broadcastTo(mockConsole);
+			expect(mockConsole.error).toHaveBeenCalledWith(sampleEvent3.message);
+		});
+	});
+
+	describe('truncate', () => {
+
+		it('shortens the length of strings that exceed the given target length', () => {
+			expect(LogEvent.truncate('hello', 5)).toBe('hello');
+			expect(LogEvent.truncate('hello', 4)).toBe('hell...');
+		});
+	});
+
+	describe('stringifySafe', () => {
+
+		it('attempts to stringify an object, but does not explode on error', () => {
+			expect(LogEvent.stringifySafe({ hello: 'test' })).toBe('{"hello":"test"}');
+			const circularObject = { parent: null };
+			circularObject.parent = circularObject;
+			expect(LogEvent.stringifySafe(circularObject)).toBe('[object Object]');
+		});
 	});
 });
