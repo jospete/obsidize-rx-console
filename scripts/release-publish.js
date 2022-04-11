b1215a15ba58343aa824024fdfb5aff2bc448f61
Josh Noel <obsidize@gmail.com>
2022-04-11 10:27:10 -0500

    normalize build scripts

--- scripts/release-publish.js
+++ scripts/release-publish.js
@@ -11,26 +11,29 @@ const git = (cmd) => {
 	return smokeTest ? 0 : execSync(fullCmd, { stdio: 'inherit' });
 };
 
-const versionTag = 'v' + version;
-const releaseBranchName = 'release/' + versionTag;
-const gitStatusOutput = execSync('git status').toString();
-const currentBranchNameMatch = /On branch (\S+)/.exec(gitStatusOutput);
+const run = () => {
 
-if (!currentBranchNameMatch) {
-	console.error('failed to match current branch from gitStatusOutput = ', gitStatusOutput);
-	console.log('currentBranchNameMatch = ', currentBranchNameMatch);
-	process.exit(1);
-}
+	const versionTag = 'v' + version;
+	const releaseBranchName = 'release/' + versionTag;
+	const gitStatusOutput = execSync('git status').toString();
+	const [currentBranchNameMatch, currentBranchName] = (/On branch (\S+)/.exec(gitStatusOutput) || []);
 
-const currentBranchName = currentBranchNameMatch[1];
+	if (!currentBranchNameMatch) {
+		console.error('failed to match current branch from gitStatusOutput = ', gitStatusOutput);
+		console.log('currentBranchNameMatch = ', currentBranchNameMatch);
+		process.exit(1);
+	}
 
-git('stash');
-git('checkout -b ' + releaseBranchName);
-git('stash apply');
-git('add --all');
-git('commit -m ' + versionTag);
-git('tag ' + versionTag);
-git('push -u origin --tags ' + releaseBranchName);
-git('checkout ' + currentBranchName);
-git('merge ' + releaseBranchName);
-git('push origin ' + currentBranchName);
+	git('stash');
+	git('checkout -b ' + releaseBranchName);
+	git('stash apply');
+	git('add --all');
+	git('commit -m ' + versionTag);
+	git('tag ' + versionTag);
+	git('push -u origin --tags ' + releaseBranchName);
+	git('checkout ' + currentBranchName);
+	git('merge ' + releaseBranchName);
+	git('push origin ' + currentBranchName);
+};
+
+run();
